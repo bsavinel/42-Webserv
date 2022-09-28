755f0f89f296c0d138b105f4fbd1df04a5c92452
Nicolas_XPS <nicolasburat1607@gmail.com>
2022-09-28 13:41:26 +0200

    WIP same old thing

--- srcs/parser.cpp
+++ srcs/parser.cpp
@@ -83,18 +83,110 @@ std::vector<std::string> split_vector(std::string str, std::string delimiter)
 	return(splitted);
 }
 
+int	checkNorme(std::vector<std::string>::iterator it, std::vector<std::string> & splitted)
+{
+	if ((*it).compare("{") != 0)
+	{
+		std::cout << "0" << std::endl;
+		return(false);
+	}
+
+	while (it != splitted.end() && (*it).compare("}") != 0)
+	{
+		if ((*it).compare("location") == 0)
+		{
+			if ((*(it + 2)).compare("{") == 0)
+			{
+				while (it != splitted.end() && (*it).compare("}") != 0)
+					it++;
+				if (it == splitted.end())
+				{
+					std::cout << "1" << std::endl;
+					return(false);
+				}
+			}
+			else 
+			{
+				std::cout << *(it + 2) << std::endl;
+				std::cout << "2" << std::endl;
+				return (false);
+			}
+		}
+		it++;
+	}
+	if (it == splitted.end())
+	{
+		std::cout << "3" << std::endl;
+		return (false);
+	}
+	return (true);
+}
+/*
+int	checkNorme(std::vector<std::string>::iterator beg, std::vector<std::string> & splitted)
+{
+	std::vector<std::string>::iterator tmp = beg;
+	int	nb_open_brackets = 0;
+	int nb_close_brackets = 0;
+
+	std::cout << "Debut" << std::endl;
+	while(tmp != splitted.end())
+	{
+		if((*tmp).compare("{") == 0)
+		{
+			std::cout << "open bra ++" << std::endl;
+			nb_open_brackets++;
+		}
+		else if((*tmp).compare("}") == 0)
+		{
+			std::cout << "close bra ++" << std::endl;
+			nb_close_brackets++;
+		}
+		tmp++;
+	}
+	std::cout << "fin de boucle" << std::endl;
+	std::cout <<"nb open brackets " << nb_open_brackets << std::endl;
+	if(nb_close_brackets == nb_open_brackets)
+		return (1);
+	return (0);
+}
+*/
+// void getServerBlock(std::vector<std::string>::iterator beg, std::vector<std::string>::iterator & stop, std::vector<std::string> splitted)
+// {
+// 	(void) stop;
+// 	std::vector<std::string>::iterator tmp;
+// 	int	nb_o_brackets = 0;
+// 	for (tmp = beg; tmp != splitted.end(); tmp++)
+// 	{
+// 		if(*tmp == "{")
+// 			nb_o_brackets++;
+// 	}
+// 	std::cout <<"nb open brackets " << nb_o_brackets << std::endl;
+// }
+
 void parser(char *config_file)
 {
+	//s_config configuration;
+
 	std::string content_file = read_file(config_file);
 	remove_comment(content_file);
 	remove_nl(content_file);
 
 	std::string delimiter(" \t;{}");
 	std::vector<std::string> splitted = split_vector(content_file, delimiter);
+	std::vector<std::string>::iterator stop;
 
-	// for(std::vector<std::string>::iterator beg = splitted.begin(); beg != splitted.end(); beg++)
-	// 	std::cout << *beg << std::endl;
-
+	for(std::vector<std::string>::iterator beg = splitted.begin(); beg != splitted.end(); beg++)
+	{
+		if ((*beg).compare("server") == 0 && (*(beg + 1)).compare("{") == 0)
+		{
+			stop = beg + 1;
+			for(std::vector<std::string>::iterator beg = splitted.begin(); beg != splitted.end(); beg++)
+				std::cout << *beg << std::endl;
+			int check = checkNorme(beg + 1, splitted);
+			if (check)
+				std::cout << "OK" << std::endl;
+	}
+	}
 	/* 
 		- iterer sur splitted
 		- detecter le mot server
