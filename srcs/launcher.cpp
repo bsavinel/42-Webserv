fd499dce872a2c48779e23d0c5a2203a07b0b20e
Nicolas BURAT DE GURGY <nburat-d@e3r1p2.clusters.42paris.fr>
2022-10-13 12:56:05 +0200

    ADD throw error when two servers are set on the same port

--- srcs/launcher.cpp
+++ srcs/launcher.cpp
@@ -21,12 +21,36 @@ static void routine(Epoll &epoll)
 	}
 }
 
+bool	check_server_port(Config &configuration)
+{
+	std::list<Server*>::iterator itServer = (configuration.getServersList()).begin();
+	std::list<Server*>::iterator itNextServer;
+	std::list<Server*>::iterator ite =  (configuration.getServersList()).end();
+	while (itServer != ite)
+	{
+		int port = (*itServer)->getPort();
+		itNextServer = itServer;
+		itNextServer++;
+		while (itNextServer != ite)
+		{
+			if (port == (*itNextServer)->getPort())
+				return(0);
+			itNextServer++;
+		}
+		itServer++;
+	}
+	return(1);
+}
+
 void launcher(char *av)
 {
 	Epoll	epoll;
 	Config configuration(av);
 
 	configuration.print_all_conf();
+
+	if(!check_server_port(configuration))
+		throw exceptWebserv("Error : Two servers set on the same port");
 
 	std::list<Server*>::iterator itServer = (configuration.getServersList()).begin();
 	while(itServer != (configuration.getServersList()).end())
