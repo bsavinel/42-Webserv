5a3981f30c9d5f2456c173b1374218704ab74cb6
Rodolphe POTTIER <rpottier@e1r1p7.clusters.42paris.fr>
2022-10-14 08:55:54 +0200

    WIP autoindex

--- includes/Event.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef EVENT_HPP
-# define EVENT_HPP
-
-#include "Epoll.hpp"
-
-void	clientEvent(Epoll &epoll, std::map<t_socket, HttpManager> &stockManager);
-void	serverEvent(Epoll &epoll, std::map<t_socket, HttpManager> &stockManager);
-
-#endif
--- includes/launcher.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef LAUNCHER_HPP
-# define LAUNCHER_HPP
-
-void launcher(char *av);
-
-#endif
