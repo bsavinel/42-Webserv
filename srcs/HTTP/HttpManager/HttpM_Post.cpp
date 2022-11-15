a9357b4613f3b96346596f47bcdaeaa134d73f0f
Rodolphe POTTIER <rpottier@e3r3p17.clusters.42paris.fr>
2022-11-15 16:20:50 +0100

    WIP need to chec kwhat happen when LEN_TO_READ is shorter than full request

--- srcs/HTTP/HttpManager/HttpM_Post.cpp
+++ srcs/HTTP/HttpManager/HttpM_Post.cpp
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <fstream>
 
+// probleme quand LEN_TO_READ est trop petit pour lire toute la requete
 
 
 void				printAscii(std::string str);
@@ -76,153 +77,90 @@ void HttpManager::parseMultiPart(std::fstream &fstream)
 	std::string			boundaryHeader;
 	t_multipart_param	multipart_param;
 
-	std::cout << "_new_process = " << _new_process << std::endl;
-
-	if (_new_process == false)
+	while (fstream.eof() != true && _process.boundaryEnd == false)
 	{
-		_process = createProcess();
-		_new_process = true;
-	}
+		std::cout << "_new_process = " << _new_process << std::endl;
 
-	std::cout << "_new_process = "<< _new_process << std::endl;
-	std::cout << "_process.boundaryStart = "<< _process.boundaryStart << std::endl;
-	if (_process.boundaryStart == false)
-	{
-		std::cout << "0" << std::endl;
-		getline(fstream, str);
-		// if ( str.size() && str[str.size()-1] == '\r' )
-		// 	str = str.substr( 0, str.size() - 1 );
-		// else
-		// 	str = str;
-		std::cout << "HEEEEEERRRRRREEE : " << str << std::endl;
-//		usleep(100000);
-		// exit (0);
-		// on trouve un block boundary,
-		if (str.compare(BoundaryStartToFind) == 0)
+		if (_new_process == false)
 		{
-			_process.boundaryStart = true;
-
-			std::cout << "_process.boundaryStart == true" << std::endl;
-	//		return ;
+		std::cout << "_new_process = "<< _new_process << std::endl;
+			_process = createProcess();
+			_new_process = true;
 		}
-	}
-	while (fstream.eof() != true && _process.boundaryStart == true && _process.header == false)
-	{
-		std::cout << "1" << std::endl;
-		getline(fstream, str);
-		if (str.compare("\r") == 0)
-		{
 
-			std::cout << "1.1" << std::endl;
-			multipart_param = getParamBoundary(boundaryHeader);
-			_process.header = true;
-			std::cout << "multipart_param.fileName.first:" << multipart_param.fileName.first << std::endl;
-			
- 			std::string fileName = _request.getLocation()->getUploadDirectory() + multipart_param.fileName.first;
-
-			std::cout << "fileName" << fileName << std::endl;
-			_uploaded.open(fileName.c_str(), std::fstream::app | std::fstream::in | std::fstream::out);
-			if (_uploaded.fail())
-				std::cout << "failed open _uploadFile" << std::endl;
-			std::cout << "multipart_param.fileName.first:" << multipart_param.fileName.first << std::endl;
-		}
-		else
+		std::cout << "_process.boundaryStart = "<< _process.boundaryStart << std::endl;
+		if (_process.boundaryStart == false)
 		{
-			std::cout << "1.2" << std::endl;
-//			boundaryHeader += str;
-			boundaryHeader.append(str, 0, str.length());
-			str.clear();
-		}
-	}
-	if (_process.header == true)
-	{
-		while (fstream.eof() != true && str.compare(BoundaryEndtoFind) != 0)
-		{
+			std::cout << "Searching BoundaryStartToFind..." << std::endl;
 			getline(fstream, str);
-			// if ( str.size() && str[str.size()-1] == '\r' )
-			// 	str = str.substr( 0, str.size() - 1 );
-			// else
-			// 	str = str;
-			if (str.compare(BoundaryEndtoFind) != 0)
-				_uploaded << str.c_str();
-			
+			// on trouve un block boundary,
+			if (str.compare(BoundaryStartToFind) == 0)
+			{
+				_process.boundaryStart = true;
+				std::cout << "BoundaryStartToFind is found!" << std::endl;
+			}
+		}
+		// on veut ici recuperer ce qui est lu, jusqu 'a trouver un BoundaryEndtoFind ou une ligne ne se terminant pas \n
+		while (fstream.eof() != true && _process.boundaryStart == true && _process.header == false)
+		{
+			std::cout << "1: " << str << std::endl;
+			getline(fstream, str);
+			if (str.compare("\r") == 0)
+			{
+				std::cout << "1.1" << std::endl;
+				multipart_param = getParamBoundary(boundaryHeader);
+				_process.header = true;
+				
+				std::string fileName = _request.getLocation()->getUploadDirectory() + multipart_param.fileName.first;
+
+				_uploaded.open(fileName.c_str(), std::fstream::app | std::fstream::in | std::fstream::out);
+				if (_uploaded.fail())
+					std::cout << "failed open _uploadFile" << std::endl;
+				std::cout << "multipart_param.fileName.first:" << multipart_param.fileName.first << std::endl;
+			}
+			else
+			{
+				std::cout << "1.2" << str << std::endl;
+	//			boundaryHeader += str;
+				boundaryHeader.append(str, 0, str.length());
+				str.clear();
+			}
+		}
+		if (_process.header == true)
+		{
+			while (fstream.eof() != true && str.compare(BoundaryEndtoFind) != 0 && str.compare(BoundaryStartToFind) != 0)
+			{
+				getline(fstream, str);
+				if (str.compare(BoundaryEndtoFind) != 0 && str.compare(BoundaryStartToFind) != 0)
+				{
+					_uploaded << str.c_str();
+					_uploaded << "\n";
+				}
+			}
 			std::cout << "2" << std::endl;
 		}
-	}
-	if (str.compare(BoundaryEndtoFind) == 0)
-	{
-		_process.boundaryEnd = true;
-		_uploaded.close();
-		_new_process = false;
+		if (str.compare(BoundaryEndtoFind) == 0 || str.compare(BoundaryStartToFind) == 0)
+		{
+			_uploaded.close();
+			_new_process = false;
+			if (str.compare(BoundaryStartToFind) == 0)
+			{
+				_process = createProcess();
+				_new_process = true;
+				_process.boundaryStart = true;
+				boundaryHeader.clear();
+//				getline(fstream, str);
+				std::cout << "BoundaryStartToFind is found!" << std::endl;
+//				parseMultiPart(fstream);
+			}
+			else
+			{
+				_process.boundaryEnd = true;
+				std::cout << "BoundaryEndToFind is found!" << std::endl;
+			}
+		}
 	}
 }
-
-
-// void HttpManager::parseMultiPart(std::fstream &fstream)
-// {
-// 	fstream.seekg(std::fstream::beg);
-
-// 	t_multipart_param	multipart_param;
-// 	t_process			process = createProcess();
-// 	int					multipart_param_is_set = false;
-// 	std::string			str;
-// 	std::string			BoundaryStartToFind = "--" + _request.getBoundary().first + "\r";
-// 	std::string			BoundaryEndtoFind = "--" + _request.getBoundary().first + "--\r" ;
-// 	std::string			boundaryHeader;
-// 	std::string			boundaryBody;
-
-// 	std::cout << "Start multiparse" << std::endl;
-
-// 	while (fstream.eof() != true)
-// 	{
-// 		getline(fstream, str);
-// 		// on trouve un block boundary,
-
-// 		if (str.compare(BoundaryStartToFind) == 0)
-// 		{
-// 			//  on doit dabord récupérer le header
-// 			getline(fstream, str);
-// 			while (str.compare("\r") != 0)
-// 			{
-// //				std::cout << "str :" << str << std::endl;
-// //				printAscii(str);
-// 				if ( str.size() && str[str.size()-1] == '\r' )
-// 					str = str.substr( 0, str.size() - 1 );
-// 				else
-// 					str = str;
-					
-// 				boundaryHeader += str;
-// 				str.clear();
-// 				getline(fstream, str);
-// 			}
-
-// 			// std::cout << "boundaryHeader/" << std::endl;
-// 			// std::cout << boundaryHeader << std::endl;
-// 			// std::cout << "/boundaryHeader" << std::endl;
-// 			multipart_param = getParamBoundary(boundaryHeader);
-
-// 			std::cout << "multipart_param/" << std::endl;
-// 			printMultiPartParam(multipart_param);
-// 			std::cout << "/multipart_param" << std::endl;
-// 			std::fstream finalFile;
-// 			std::string fileName = _request.getLocation()->getUploadDirectory() + multipart_param.fileName.first;
-// 			std::cout << "filename: " << fileName << std::endl;
-// 			finalFile.open(fileName.c_str(), std::fstream::app | std::fstream::in | std::fstream::out);
-			
-// 			while (str.compare(BoundaryEndtoFind) != 0)
-// 			{
-// 				std::cout << "TEMOIN" << std::endl;
-// 				finalFile << str.c_str();
-// //				boundaryBody += str;
-// 				getline(fstream, str);
-// 			}
-// 		}
-// 	}
-
-// 	std::cout << "End multiparse" << std::endl;
-// }
-
-
 
 void printAscii(std::string str)
 {
@@ -249,10 +187,10 @@ t_multipart_param	getParamBoundary(std::string boundaryHeader)
 	std::string::size_type endPos = 0;
 	std::string::size_type subStrLen = 0;
 
-	std::cout << "boundaryHeader/" << std::endl;
-	// std::cout << boundaryHeader << std::endl;
-	printAscii(boundaryHeader);
-	std::cout << "/boundaryHeader" << std::endl;
+//	std::cout << "boundaryHeader/" << std::endl;
+//	std::cout << boundaryHeader << std::endl;
+//	printAscii(boundaryHeader);
+//	std::cout << "/boundaryHeader" << std::endl;
 
 	toFind = "Content-Disposition: ";
 	
